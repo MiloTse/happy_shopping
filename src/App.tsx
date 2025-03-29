3db0085db3e165d7a92f760de690e1c34b3a860e
HuacongXie <solarsys@outlook.com>
2025-03-29 17:11:51 -0400

    1.added logo and title

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import './styles/app.css';
 
 const App =() => {
   return (
-    <div className="page">
-
+    <div className="page guide-page">
+      <img alt="logo"
+        className="main-pic"
+        src={require('./images/halg_logo_icon_@2x.png')}
+        />
+        <p className="title">Happy Shopping</p>
     </div>
   );
 }
