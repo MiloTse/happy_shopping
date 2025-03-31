f2eabe9fda1fc564228a012f2a2610cf878cdda0
HuacongXie <solarsys@outlook.com>
2025-03-30 22:59:55 -0400

    1.imported HashRouter, Routes, Route in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import 'normalize.css';
 import './styles/border.css';
 import './styles/base.css';
 import Guide from './containers/Guide/Guide';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
 
 const App =() => {
 
     return (
-        <Guide />
-    )
+        <HashRouter>
+            <Routes>
+                <Route path="/#" element={<Guide />} />
+            </Routes>
+        </HashRouter>
+     )
 }
 
 export default App;
