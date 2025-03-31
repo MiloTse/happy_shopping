a8ff0d86bb3f2e966c00866ed4b4ef10199be86e
HuacongXie <solarsys@outlook.com>
2025-03-31 00:05:29 -0400

    1.defined a custom hook to handle animation

--- src/containers/Guide/Guide.tsx
+++ src/containers/Guide/Guide.tsx
@@ -2,14 +2,28 @@ import React, {useCallback, useEffect, useRef} from 'react';
 import './style.css';
 import {useNavigate} from "react-router-dom";
 
-const Guide =() => {
+//define a custom hook to handle animation
+const useRefAnimation = ()=> {
     //处理动画相关的逻辑
     //handle the animation related logic
     const ref = useRef<HTMLDivElement>(null!);
     useEffect(()=>{
         ref.current.style.opacity = '1';
-    })
+    },[]);
+    return ref;
+}
 
+
+
+const Guide =() => {
+    //处理动画相关的逻辑
+    //handle the animation related logic
+/*    const ref = useRef<HTMLDivElement>(null!);
+    useEffect(()=>{
+        ref.current.style.opacity = '1';
+    })*/
+    //use custom hook to handle animation
+    const ref = useRefAnimation();
 
     //处理页面跳转相关的逻辑
     //handle the page jump related logic
