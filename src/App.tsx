b451e17cca26af3cdb95395a70f9f67e65be89f3
HuacongXie <solarsys@outlook.com>
2025-03-30 23:06:58 -0400

    1.added Route to Login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import 'normalize.css';
 import './styles/border.css';
 import './styles/base.css';
-import Guide from './containers/Guide/Guide';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import React from "react";
+import Guide from './containers/Guide/Guide';
+import Login from './containers/Login/Login';
+
 
 const App =() => {
 
     return (
         <HashRouter>
             <Routes>
-                <Route path="/#" element={<Guide />} />
+                <Route path="/" element={<Guide />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
         </HashRouter>
      )
