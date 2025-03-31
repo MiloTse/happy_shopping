95c061d6606479326904298e4ea6bf972e167183
HuacongXie <solarsys@outlook.com>
2025-03-30 21:54:11 -0400

    1.added logo and title slogan_word_icon_@2x.png 2.added styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import './styles/app.css';
 
 const App =() => {
   return (
-    <div className="page guide-page">
-      <img alt="logo"
-        className="main-pic"
-        src={require('./images/halg_logo_icon_@2x.png')}
-        />
-        <p className="title">Happy Shopping</p>
-    </div>
+      <div className="page guide-page">
+          <img alt="logo"
+               className="main-pic"
+               src={require('./images/halg_logo_icon_@2x.png')}
+          />
+          <p className="title">Happy Shopping</p>
+          <img alt="logo"
+               className="sub-pic"
+               src={require('./images/slogan_word_icon_@2x.png')}
+          />
+      </div>
   );
 }
 
