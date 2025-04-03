9846c948ec651c79183b72f276c6c033c9264fa8
HuacongXie <solarsys@outlook.com>
2025-04-02 23:29:58 -0400

    1.added comments on useRequest.tsx

--- src/utils/useRequest.tsx
+++ src/utils/useRequest.tsx
@@ -1,12 +1,21 @@
 import axios, {AxiosRequestConfig, Method} from "axios";
 import {useState} from "react";
 
+//define a custom hook to send request
 function useRequest(url: string, method: Method, payload: AxiosRequestConfig) {
     const[data, setData] = useState(null);
-    const[error, setError] = useState(null);
+    const[error, setError] = useState('');
     const[loaded, setLoaded] = useState(false);
 
     const request = async () => {
+        //每次请求之前先清空之前的状态
+        //clear the previous state before each request
+        setData(null);
+        setError('');
+        setLoaded(false);
+
+        //发送异步请求，捕捉异常
+        //sending a request, catching an exception
         try{
             const response = await axios.request({
                 //passing three parameters as obj
@@ -16,12 +25,13 @@ function useRequest(url: string, method: Method, payload: AxiosRequestConfig) {
             });
             setData(response.data);
         }catch (e: any) {
-            console.log(e.message);
             setError(e.message);
         }finally {
             //no matter success or fail
             setLoaded(true);
         }
+        //return
+        return {data, error, loaded, request};
     }
 
 
