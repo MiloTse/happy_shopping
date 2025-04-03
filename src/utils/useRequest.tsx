ef461017a11e0ed6038e99664d9b567e0c9b5438
HuacongXie <solarsys@outlook.com>
2025-04-02 23:24:21 -0400

    1.added try/catch/finally on useRequest.tsx

--- src/utils/useRequest.tsx
+++ src/utils/useRequest.tsx
@@ -1,10 +1,30 @@
-import {AxiosRequestConfig} from "axios";
+import axios, {AxiosRequestConfig, Method} from "axios";
 import {useState} from "react";
 
-function useRequest(url: string, config: AxiosRequestConfig) {
+function useRequest(url: string, method: Method, payload: AxiosRequestConfig) {
     const[data, setData] = useState(null);
     const[error, setError] = useState(null);
     const[loaded, setLoaded] = useState(false);
+
+    const request = async () => {
+        try{
+            const response = await axios.request({
+                //passing three parameters as obj
+                url,
+                method,
+                data: payload
+            });
+            setData(response.data);
+        }catch (e: any) {
+            console.log(e.message);
+            setError(e.message);
+        }finally {
+            //no matter success or fail
+            setLoaded(true);
+        }
+    }
+
+
 }
 
 
