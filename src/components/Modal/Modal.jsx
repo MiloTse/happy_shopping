e29f851e82ece03bb7ae3ede8105e3c52cc4366e
HuacongXie <solarsys@outlook.com>
2025-04-06 23:18:55 -0400

    1.adjusted Modal.jsx and style.scss

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,7 +3,7 @@ import './style.scss';
 function Modal() {
     return (
         <div className="modal">
-            <div className="modal-content">
+            <div className="modal-text">
                 <span className="close">&times;</span>
                 <p>Apps Error occurred...</p>
             </div>
