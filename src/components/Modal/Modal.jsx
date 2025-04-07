ba2d0094ee1f3a7b5e05117d137ed910a2821874
HuacongXie <solarsys@outlook.com>
2025-04-06 22:58:50 -0400

    1.created component Modal.jsx

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,13 @@
+
+function Modal() {
+    return (
+        <div className="modal">
+            <div className="modal-content">
+                <span className="close">&times;</span>
+                <p>Apps Error occurred...</p>
+            </div>
+        </div>
+    );
+}
+
+export default Modal;
