f35c95fe6c7567f7ac396477859dae8a5daf8bd4
HuacongXie <solarsys@outlook.com>
2025-04-08 11:53:20 -0400

    1.dynamic append div attribute to show color

--- src/containers/Account/Account.tsx
+++ src/containers/Account/Account.tsx
@@ -2,17 +2,25 @@ import './style.scss'
 import React, {useEffect, useRef, useState} from "react";
 import useRequest from "../../utils/useRequest";
 import Modal,{ ModalInterfaceType } from "../../components/Modal/Modal";
-import {Link, Outlet} from "react-router-dom";
+import {Link, Outlet, useLocation} from "react-router-dom";
 
 
 const Account = ()=> {
+
+    const location = useLocation();
+    console.log(location);
+    const isLoginActivated = location.pathname === '/account/login';
+    const loginActiveClass = isLoginActivated ? 'tab-item-active' : '';
+    const registerActiveClass = !isLoginActivated ? 'tab-item-active' : '';
+
+
     return (
         <div className="page account-page">
             <div className="tab">
-                <div className="tab-item tab-item-left">
+                <div className={`tab-item tab-item-left ${loginActiveClass}`}>
                     <Link to="/account/login">login</Link>
                 </div>
-                <div className="tab-item tab-item-right">
+                <div className={`tab-item tab-item-right ${registerActiveClass}`}>
                     <Link to="/account/register">register</Link>
                 </div>
             </div>
