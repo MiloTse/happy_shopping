3e35bbc16deff1e3d94f9a1c0ff0d3925901eb07
HuacongXie <solarsys@outlook.com>
2025-04-08 12:00:10 -0400

    1.created Home.tsx

---
+++ src/containers/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React, {useCallback, useEffect, useRef} from 'react';
+import './style.scss';
+
+
+const Home =() => {
+
+
+    return (
+        <div>
+            Home Page
+        </div>
+    )
+}
+
+export default Home;
