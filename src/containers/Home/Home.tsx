6a80d6be4636f8c2297371e61f18f5812d769e6c
HuacongXie <solarsys@outlook.com>
2025-04-10 13:24:03 -0400

    1.added style and location icon for home page

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -6,8 +6,18 @@ const Home =() => {
 
 
     return (
-        <div>
-            Home Page
+        <div className="page home-page">
+            <div className="banner">
+                <h3 className="location">
+                    <span className="iconfont">&#xe68e;</span>
+                    Ottawa(Chinatown Branch)
+                </h3>
+                <div className="search">
+                <span className="iconfont"></span>
+                    Input search words here
+                </div>
+            </div>
+
         </div>
     )
 }
