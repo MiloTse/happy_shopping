b4cf9ad73ca84174ac652ee19e19ec25fe9b87f2
HuacongXie <solarsys@outlook.com>
2025-04-15 00:11:34 -0400

    1.extracted Card Div on Home.tsx to Card.tsx

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -83,41 +83,7 @@ const Home =() => {
         <div className="page home-page">
             <Banner location={data?.data.location} banners={data?.data.banners}/>
             <Category categories={data?.data.categories}/>
-            <div className="card">
-                <h3 className="card-title">
-                    <img
-                        alt="新品尝鲜"
-                        className="card-title-img"
-                        src="http://statics.dell-lee.com/shopping/hot.png"/>
-                    新品尝鲜
-                <div className="card-title-more">
-                    more
-                    <span className="iconfont">&#xe70d;</span>
-                </div>
-                </h3>
-                <div className="card-content">
 
-                    {
-                        (data?.data.fresh || []).map((item)=> {
-                            return (
-                                <div className="card-content-item" key={item.id}>
-                                    <img
-                                        alt={item.name}
-                                        className="card-content-item-img"
-                                        src={item.imgUrl}/>
-                                    <p className='card-content-item-desc'>{item.name}</p>
-                                    <div className='card-content-item-price'>
-                                        <span className='card-content-item-yen'>&yen;</span>
-                                        {item.price}
-                                        <div className='iconfont'>&#xe6b2;</div>
-                                    </div>
-                                </div>
-                            )
-                        })
-                    }
-
-                </div>
-            </div>
             <div className="bottom">
              -- I am the bottom line --
             </div>
--- src/containers/Home/components/Card.tsx
+++ src/containers/Home/components/Card.tsx
@@ -1,8 +1,41 @@
+import React from "react";
 
 const Card = () => {
     return (
-        <div>
-            Card
+        <div className="card">
+            <h3 className="card-title">
+                <img
+                    alt="新品尝鲜"
+                    className="card-title-img"
+                    src="http://statics.dell-lee.com/shopping/hot.png"/>
+                新品尝鲜
+                <div className="card-title-more">
+                    more
+                    <span className="iconfont">&#xe70d;</span>
+                </div>
+            </h3>
+            <div className="card-content">
+
+                {
+                    (data?.data.fresh || []).map((item) => {
+                        return (
+                            <div className="card-content-item" key={item.id}>
+                                <img
+                                    alt={item.name}
+                                    className="card-content-item-img"
+                                    src={item.imgUrl}/>
+                                <p className='card-content-item-desc'>{item.name}</p>
+                                <div className='card-content-item-price'>
+                                    <span className='card-content-item-yen'>&yen;</span>
+                                    {item.price}
+                                    <div className='iconfont'>&#xe6b2;</div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+
+            </div>
         </div>
     )
 }
