09c841e9393068a578f04030eacce87667399708
HuacongXie <solarsys@outlook.com>
2025-04-14 23:48:28 -0400

    1.extracted types in types.ts

--- src/containers/Home/types.ts
+++ src/containers/Home/types.ts
@@ -1,20 +1,31 @@
+
+
+
+export type LocationType = Array<{
+    id: string;
+    address: string;
+}>
+export type BannersType = Array<{
+    id: string,
+    imgUrl: string
+}>
+
+//分类信息类型
+export type CategoriesType = Array<{
+    id: string,
+    name: string,
+    imgUrl: string
+}>
+
+
 //返回内容类型
 export type ResponseType = {
     success: boolean;
     data: {
-        location: {
-            id: string;
-            address: string;
-        };
-        banners: Array<{
-            id: string,
-            imgUrl: string
-        }>
-        categories: Array<{
-            id: string,
-            name: string,
-            imgUrl: string
-        }>
+        location: LocationType;
+        banners: BannersType;
+        categories: CategoriesType;
+
         fresh: Array<{
             id: string,
             name: string,
