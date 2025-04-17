1abd2099fda691ad6c4085f3b687aa4e075d3362
HuacongXie <solarsys@outlook.com>
2025-04-16 23:54:12 -0400

    1.added Nearby.tsx

---
+++ src/containers/Nearby/Nearby.tsx
@@ -0,0 +1,12 @@
+import './style.scss';
+
+const Nearby = () => {
+    return (
+        <div className='nearby'>
+            <div className='nearby-title'>
+            </div>
+        </div>
+    )
+}
+
+export default Nearby;
