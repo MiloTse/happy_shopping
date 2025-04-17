30967a4cb9f1c378949b86432b415bdab0468b33
HuacongXie <solarsys@outlook.com>
2025-04-17 19:13:46 -0400

    1.added ui for Search.tsx

--- src/containers/Search/Search.tsx
+++ src/containers/Search/Search.tsx
@@ -1,10 +1,40 @@
-
+import './style.scss';
 
 const Search = () => {
 
     return (
-        <div>
-            Search Page
+        <div className="page search-page">
+            <div className="search">
+                <div className="search-back iconfont">&#xe6a9;</div>
+                <div className="search-area">
+                    <div className="search-icon iconfont">&#xe600;</div>
+                    <input className="search-input" placeholder="Please enter product name"/>
+                </div>
+
+            </div>
+            <div className="title">
+                History Search
+                <div className="iconfont title-close">&#xe610;</div>
+            </div>
+            <ul className="list">
+                <li className='list-item'>shrimp ball</li>
+                <li className='list-item'>pork</li>
+                <li className='list-item'>chicken</li>
+                <li className='list-item'>chip</li>
+                <li className='list-item'>banana</li>
+                <li className='list-item'>steak</li>
+            </ul>
+            <div className="title">
+                Hot Search
+            </div>
+            <ul className="list">
+                <li className='list-item'>shrimp ball</li>
+                <li className='list-item'>pork</li>
+                <li className='list-item'>chicken</li>
+                <li className='list-item'>chip</li>
+                <li className='list-item'>banana</li>
+                <li className='list-item'>steak</li>
+            </ul>
         </div>
     );
 };
