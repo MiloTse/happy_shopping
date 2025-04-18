bf5b800f0a017a5729545643fecf853c7e135f5a
HuacongXie <solarsys@outlook.com>
2025-04-18 00:56:11 -0400

    1.added div content in SearchList.tsx

--- src/containers/SearchList/SearchList.tsx
+++ src/containers/SearchList/SearchList.tsx
@@ -1,9 +1,26 @@
+import React from "react";
+import './style.scss';
+import {Link} from "react-router-dom";
+
+
 const SearchList = () => {
+
     return (
-        <div>
-            SearchList
+        <div className="page search-list-page">
+            <div className='search'>
+                <Link to='/home' className='search-back-link'>
+                    <div className='search-back-iconiconfont'>&#xe601;</div>
+                </Link>
+                <div className='search-area'>
+                    <div className='search-iconiconfont'>&#xe64e; </div>
+                    <input
+                        className='search-input'
+                        placeholder='Please enter product name！'
+                    />
+                </div>
+            </div>
         </div>
-    );
+    )
 }
 
 export default SearchList;
