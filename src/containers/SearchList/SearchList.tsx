3912b19c1f4ba6540fda4375e3eed329dd2a76f6
HuacongXie <solarsys@outlook.com>
2025-04-18 12:40:26 -0400

    1.added search-close icon on SearchList.tsx and adjusted style

--- src/containers/SearchList/SearchList.tsx
+++ src/containers/SearchList/SearchList.tsx
@@ -1,23 +1,24 @@
-import React from "react";
+import React from 'react';
 import './style.scss';
-import {Link} from "react-router-dom";
+import {Link} from 'react-router-dom';
 
 
 const SearchList = () => {
 
     return (
         <div className="page search-list-page">
-            <div className='search'>
-                <Link to='/home' className='search-back-link'>
-                    <div className='search-back-iconiconfont'>&#xe601;</div>
+            <div className="search">
+                <Link to="/home" className="search-back-link">
+                    <div className="search-back-icon iconfont">&#xe6a9;</div>
                 </Link>
-                <div className='search-area'>
-                    <div className='search-iconiconfont'>&#xe64e; </div>
+                <div className="search-area">
+                    <div className="search-icon iconfont">&#xe600;</div>
                     <input
-                        className='search-input'
-                        placeholder='Please enter product name！'
+                        className="search-input"
+                        placeholder="Please enter product name"
                     />
                 </div>
+                <div className="search-close iconfont">&#xe610;</div>
             </div>
         </div>
     )
