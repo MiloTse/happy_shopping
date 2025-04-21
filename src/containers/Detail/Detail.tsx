bf574c856ea96f44335f80d9416608a0d1b0b756
HuacongXie <solarsys@outlook.com>
2025-04-20 23:28:55 -0400

    1.created title element

--- src/containers/Detail/Detail.tsx
+++ src/containers/Detail/Detail.tsx
@@ -1,11 +1,17 @@
 
 import './style.scss';
+import React from "react";
 
 const Detail = () => {
 
     return (
         <div className="page detail-page">
-            Detail
+            <div className="title">
+                <div className="iconfont">&#xe6a9;</div>
+                <div className="text">Detail</div>
+            </div>
+
+
         </div>
     )
 
