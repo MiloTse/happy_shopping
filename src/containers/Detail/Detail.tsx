60b5c9680ea1e453a8383cc4dc9a25296e38e59f
HuacongXie <solarsys@outlook.com>
2025-04-21 17:34:56 -0400

    1.adjusted style for main-price and main-content div element

--- src/containers/Detail/Detail.tsx
+++ src/containers/Detail/Detail.tsx
@@ -14,13 +14,12 @@ const Detail = () => {
             <div className='main'>
                 <div className='main-price'><span className="main-price-symbol">&#36;</span>
                 139.9</div>
-                <div className='sales'>546</div>
-            <div className='main-content'>
+                <div className='main-sales'>sold 546</div>
+                 <div className='main-content'>
                     <div
                         className='main-content-title'>山东海阳普罗旺斯西红柿自然熟沙飘番茄新鲜水果蔬菜健康轻食严选彩箱5斤装
                     </div>
                     <p className='main-content-subtitle'>山东海阳普罗旺斯西红柿自然熟沙飘番茄新鲜水果蔬菜健康轻食严选彩箱5斤装</p>
-
                 </div>
 
             </div>
