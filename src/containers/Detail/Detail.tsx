e70ba409ba76d67a43417cbcdb22d493991ec342
HuacongXie <solarsys@outlook.com>
2025-04-20 23:24:52 -0400

    1.created Detail.tsx file

---
+++ src/containers/Detail/Detail.tsx
@@ -0,0 +1,14 @@
+
+import './style.scss';
+
+const Detail = () => {
+
+    return (
+        <div className="page detail-page">
+            Detail
+        </div>
+    )
+
+}
+
+export default Detail;
