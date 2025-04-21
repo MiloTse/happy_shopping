6a9cdf701ab6040bb1e2d5dfaec673f40dbcb80b
HuacongXie <solarsys@outlook.com>
2025-04-21 00:31:56 -0400

    1.added main-price and main-content div element

--- src/containers/Detail/Detail.tsx
+++ src/containers/Detail/Detail.tsx
@@ -10,7 +10,20 @@ const Detail = () => {
                 <div className="iconfont">&#xe6a9;</div>
                 <div className="text">Detail</div>
             </div>
-            <img className='image' alt='' src='http://statics.dell-lee.com/shopping/detail.png' />
+            <img className='image' alt='' src='http://statics.dell-lee.com/shopping/detail.png'/>
+            <div className='main'>
+                <div className='main-price'><span className="main-price-symbol">&#36;</span>
+                139.9</div>
+                <div className='sales'>546</div>
+            <div className='main-content'>
+                    <div
+                        className='main-content-title'>山东海阳普罗旺斯西红柿自然熟沙飘番茄新鲜水果蔬菜健康轻食严选彩箱5斤装
+                    </div>
+                    <p className='main-content-subtitle'>山东海阳普罗旺斯西红柿自然熟沙飘番茄新鲜水果蔬菜健康轻食严选彩箱5斤装</p>
+
+                </div>
+
+            </div>
 
         </div>
     )
