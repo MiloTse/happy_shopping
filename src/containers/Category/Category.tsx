f359072a0c84f0b7b043b42b414cdc0c403c70d2
HuacongXie <solarsys@outlook.com>
2025-04-23 00:24:26 -0400

    1.created Category.tsx

---
+++ src/containers/Category/Category.tsx
@@ -0,0 +1,13 @@
+import './style.scss';
+
+
+const Category = () => {
+
+    return (
+        <div className="page category-page">
+            Category
+        </div>
+    )
+}
+
+export default Category;
