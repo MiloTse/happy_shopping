841755de70c7496e003b36ab58bbb4d76a13c2d3
HuacongXie <solarsys@outlook.com>
2025-04-23 00:34:15 -0400

    1.added div on Category.tsx

--- src/containers/Category/Category.tsx
+++ src/containers/Category/Category.tsx
@@ -1,11 +1,29 @@
 import './style.scss';
+import React from "react";
 
 
 const Category = () => {
 
     return (
         <div className="page category-page">
-            Category
+            <div className="docker">
+                <div className="docker-item docker-item-active">
+                    <p className="iconfont">&#xe608;</p>
+                    <p className="docker-item-title">Home</p>
+                </div>
+                <div className="docker-item">
+                    <p className="iconfont">&#xe609;</p>
+                    <p className="docker-item-title">Category</p>
+                </div>
+                <div className="docker-item">
+                    <p className="iconfont">&#xe601;</p>
+                    <p className="docker-item-title">Cart</p>
+                </div>
+                <div className="docker-item">
+                    <p className="iconfont">&#xe602;</p>
+                    <p className="docker-item-title">Mine</p>
+                </div>
+            </div>
         </div>
     )
 }
