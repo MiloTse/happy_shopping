aaac23559e2556a1e2ed65ab32804061e6f5db73
HuacongXie <solarsys@outlook.com>
2025-04-25 12:47:48 -0400

    1.added <div className="product-item"

--- src/containers/Category/Category.tsx
+++ src/containers/Category/Category.tsx
@@ -59,8 +59,70 @@ const Category = () => {
                             buy
                         </div>
                     </div>
-
-
+                </div>
+                <div className="product-item">
+                    <img className="product-item-img"
+                        src="http://statics.dell-lee.com/shopping/fresh-1.png"
+                        alt=""
+                    />
+                    <div className="product-item-content">
+                        <div className="product-item-title">chicken wing middle 1000g/...</div>
+                        <div className="product-item-sales">sold 156</div>
+                        <div className="product-item-price">
+                            <span className="product-item-price-symbol">&#36;  </span>156
+                        </div>
+                        <div className="product-item-button">
+                            buy
+                        </div>
+                    </div>
+                </div>
+                <div className="product-item">
+                    <img className="product-item-img"
+                        src="http://statics.dell-lee.com/shopping/fresh-1.png"
+                        alt=""
+                    />
+                    <div className="product-item-content">
+                        <div className="product-item-title">chicken wing middle 1000g/...</div>
+                        <div className="product-item-sales">sold 156</div>
+                        <div className="product-item-price">
+                            <span className="product-item-price-symbol">&#36;  </span>156
+                        </div>
+                        <div className="product-item-button">
+                            buy
+                        </div>
+                    </div>
+                </div>
+                <div className="product-item">
+                    <img className="product-item-img"
+                        src="http://statics.dell-lee.com/shopping/fresh-1.png"
+                        alt=""
+                    />
+                    <div className="product-item-content">
+                        <div className="product-item-title">chicken wing middle 1000g/...</div>
+                        <div className="product-item-sales">sold 156</div>
+                        <div className="product-item-price">
+                            <span className="product-item-price-symbol">&#36;  </span>156
+                        </div>
+                        <div className="product-item-button">
+                            buy
+                        </div>
+                    </div>
+                </div>
+                <div className="product-item">
+                    <img className="product-item-img"
+                        src="http://statics.dell-lee.com/shopping/fresh-1.png"
+                        alt=""
+                    />
+                    <div className="product-item-content">
+                        <div className="product-item-title">chicken wing middle 1000g/...</div>
+                        <div className="product-item-sales">sold 156</div>
+                        <div className="product-item-price">
+                            <span className="product-item-price-symbol">&#36;  </span>156
+                        </div>
+                        <div className="product-item-button">
+                            buy
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="docker">
