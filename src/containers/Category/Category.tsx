f0028007d11f530ccf9f396acb4535b3f4862478
HuacongXie <solarsys@outlook.com>
2025-04-25 12:48:32 -0400

    1.added <div className="product-item" 2.added overflow-y: scroll;

--- src/containers/Category/Category.tsx
+++ src/containers/Category/Category.tsx
@@ -124,6 +124,38 @@ const Category = () => {
                         </div>
                     </div>
                 </div>
+                <div className="product-item">
+                    <img className="product-item-img"
+                        src="http://statics.dell-lee.com/shopping/fresh-1.png"
+                        alt=""
+                    />
+                    <div className="product-item-content">
+                        <div className="product-item-title">chicken wing middle 1000g/...</div>
+                        <div className="product-item-sales">sold 156</div>
+                        <div className="product-item-price">
+                            <span className="product-item-price-symbol">&#36;  </span>156
+                        </div>
+                        <div className="product-item-button">
+                            buy
+                        </div>
+                    </div>
+                </div>
+                <div className="product-item">
+                    <img className="product-item-img"
+                        src="http://statics.dell-lee.com/shopping/fresh-1.png"
+                        alt=""
+                    />
+                    <div className="product-item-content">
+                        <div className="product-item-title">chicken wing middle 1000g/...</div>
+                        <div className="product-item-sales">sold 156</div>
+                        <div className="product-item-price">
+                            <span className="product-item-price-symbol">&#36;  </span>156
+                        </div>
+                        <div className="product-item-button">
+                            buy
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className="docker">
             <div className="docker-item ">
