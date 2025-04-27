5177c4bdecfc239c61657c8e66398f550d7648b5
HuacongXie <solarsys@outlook.com>
2025-04-26 21:07:10 -0400

    1. added navigate to Home page via onClick

--- src/components/Docker/Docker.tsx
+++ src/components/Docker/Docker.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import './style.scss';
+import {useNavigate} from "react-router-dom";
 
 function Docker   ()   {
+    const navigate = useNavigate();
+
     return (
 
             <div className="docker">
-                <div className="docker-item docker-item-active">
+                <div className="docker-item docker-item-active" onClick={() => navigate('/home')}>
                     <p className="iconfont">&#xe608;</p>
                     <p className="docker-item-title">Home</p>
                 </div>
-                <div className="docker-item">
+                <div className="docker-item" >
                     <p className="iconfont">&#xe609;</p>
                     <p className="docker-item-title">Category</p>
                 </div>
