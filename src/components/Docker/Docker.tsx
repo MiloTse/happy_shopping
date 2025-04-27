c0769657fed5ba96c8f5d443159cd0416eddd2a2
HuacongXie <solarsys@outlook.com>
2025-04-26 20:58:10 -0400

    1.  moved Home.tsx docker div to Docker.tsx

--- src/components/Docker/Docker.tsx
+++ src/components/Docker/Docker.tsx
@@ -1,8 +1,27 @@
-const Docker = () => {
+import React from "react";
+
+function Docker   ()   {
     return (
-        <div>
-            Docker
-        </div>
+
+            <div className="docker">
+                <div className="docker-item docker-item-active">
+                    <p className="iconfont">&#xe608;</p>
+                    <p className="docker-item-title">Home</p>
+                </div>
+                <div className="docker-item">
+                    <p className="iconfont">&#xe609;</p>
+                    <p className="docker-item-title">Category</p>
+                </div>
+                <div className="docker-item">
+                    <p className="iconfont">&#xe601;</p>
+                    <p className="docker-item-title">Cart</p>
+                </div>
+                <div className="docker-item">
+                    <p className="iconfont">&#xe602;</p>
+                    <p className="docker-item-title">Mine</p>
+                </div>
+            </div>
+
     );
 }
 
