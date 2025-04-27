2ed4dca98a2c11023d56c3e92a8959f2bca472ad
HuacongXie <solarsys@outlook.com>
2025-04-26 20:58:24 -0400

    1.  removed Home.tsx docker div

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -106,24 +106,7 @@ const Home =() => {
             <div className="bottom">
              -- I am the bottom line --
             </div>
-            <div className="docker">
-                <div className="docker-item docker-item-active">
-                    <p className="iconfont">&#xe608;</p>
-                    <p className="docker-item-title">Home</p>
-                </div>
-                <div className="docker-item">
-                    <p className="iconfont">&#xe609;</p>
-                    <p className="docker-item-title">Category</p>
-                </div>
-                <div className="docker-item">
-                    <p className="iconfont">&#xe601;</p>
-                    <p className="docker-item-title">Cart</p>
-                </div>
-                <div className="docker-item">
-                    <p className="iconfont">&#xe602;</p>
-                    <p className="docker-item-title">Mine</p>
-                </div>
-            </div>
+
         </div>
     )
 }
