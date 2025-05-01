679a6d27bfd967d156516f5138a93181d0171833
HuacongXie <solarsys@outlook.com>
2025-05-01 00:24:32 -0400

    added div cart-page in Cart.tsx

--- src/containers/Cart/Cart.tsx
+++ src/containers/Cart/Cart.tsx
@@ -1,5 +1,13 @@
+import './style.scss';
+import Docker from "../../components/Docker/Docker";
+import React from "react";
+
 function Cart() {
-    return <div>Cart</div>;
+    return (
+        <div className="page cart-page">
+            <Docker activeName='home' />
+        </div>
+    )
 }
 
 export default Cart;
