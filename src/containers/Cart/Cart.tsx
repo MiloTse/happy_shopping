e5f647e41da58f555b42bdab089362de52780b71
HuacongXie <solarsys@outlook.com>
2025-05-02 01:49:15 -0400

    used shop.cartList to show the cart list

--- src/containers/Cart/Cart.tsx
+++ src/containers/Cart/Cart.tsx
@@ -128,69 +128,32 @@ function Cart() {
                                 {
                                     shop.cartList.map(product=>{
                                         return (
-                                            <div>test</div>
+                                            <div className='shop-product'>
+                                                <div className='radio'></div>
+                                                <img src='http://statics.dell-lee.com/shopping/category-list-1.png'
+                                                     alt=''
+                                                     className='shop-product-img'/>
+                                                <div className='shop-product-content'>
+                                                    <div className='shop-product-title'>
+                                                        潍坊水果萝卜10斤水果萝卜甜脆水果型潍县青沙窝天津萝卜…
+                                                    </div>
+
+
+                                                    <div className='shop-product-kilo'>
+                                                        0.43kg
+                                                    </div>
+                                                    <div className='shop-product-price'>
+                                                        <span className='shop-product-price-symbol'>&#36; </span>99
+                                                    </div>
+                                                    <input value='3' className='shop-product-count'/>
+                                                </div>
+                                            </div>
                                         )
                                     })
                                 }
 
 
-                                <div className='shop-product'>
-                                    <div className='radio'></div>
-                                    <img src='http://statics.dell-lee.com/shopping/category-list-1.png' alt=''
-                                         className='shop-product-img'/>
-                                    <div className='shop-product-content'>
-                                        <div className='shop-product-title'>
-                                            潍坊水果萝卜10斤水果萝卜甜脆水果型潍县青沙窝天津萝卜…
-                                        </div>
 
-
-                                        <div className='shop-product-kilo'>
-                                            0.43kg
-                                        </div>
-                                        <div className='shop-product-price'>
-                                            <span className='shop-product-price-symbol'>&#36; </span>99
-                                        </div>
-                                        <input value='3' className='shop-product-count'/>
-                                    </div>
-                                </div>
-                                <div className='shop-product'>
-                                    <div className='radio'></div>
-                                    <img src='http://statics.dell-lee.com/shopping/category-list-1.png' alt=''
-                                         className='shop-product-img'/>
-                                    <div className='shop-product-content'>
-                                        <div className='shop-product-title'>
-                                            潍坊水果萝卜10斤水果萝卜甜脆水果型潍县青沙窝天津萝卜…
-                                        </div>
-
-
-                                        <div className='shop-product-kilo'>
-                                            0.43kg
-                                        </div>
-                                        <div className='shop-product-price'>
-                                            <span className='shop-product-price-symbol'>&#36; </span>99
-                                        </div>
-                                        <input value='3' className='shop-product-count'/>
-                                    </div>
-                                </div>
-                                <div className='shop-product'>
-                                    <div className='radio'></div>
-                                    <img src='http://statics.dell-lee.com/shopping/category-list-1.png' alt=''
-                                         className='shop-product-img'/>
-                                    <div className='shop-product-content'>
-                                        <div className='shop-product-title'>
-                                            潍坊水果萝卜10斤水果萝卜甜脆水果型潍县青沙窝天津萝卜…
-                                        </div>
-
-
-                                        <div className='shop-product-kilo'>
-                                            0.43kg
-                                        </div>
-                                        <div className='shop-product-price'>
-                                            <span className='shop-product-price-symbol'>&#36; </span>99
-                                        </div>
-                                        <input value='3' className='shop-product-count'/>
-                                    </div>
-                                </div>
                             </div>
                         </div>
 
