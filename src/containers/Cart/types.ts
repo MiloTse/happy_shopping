e7cdcc5abf626cb2dc0721923b632e8dbb7dd71e
HuacongXie <solarsys@outlook.com>
2025-05-02 01:07:17 -0400

    extracted CartItemType in types.ts for Cart.tsx

--- src/containers/Cart/types.ts
+++ src/containers/Cart/types.ts
@@ -1,19 +1,18 @@
-//返回内容类型
+type CartItemType = {
+    productId: string;
+    imgUrl: string;
+    weight: string;
+    title: string;
+    price: number;
+    count: number;
+}
+
+
 export type ResponseType = {
     success: boolean,
     data:  Array<{
         shopId: string;
         shopName: string;
-        cartList: Array<{
-            productId: string;
-            imgUrl: string;
-            weight: string;
-            title: string;
-            price: number;
-            count: number;
-        }>;
-
-
-
+        cartList: Array<CartItemType>;
     }>,
 }
