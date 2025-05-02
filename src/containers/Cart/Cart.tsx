6f1e33f12dcd6dd8b8ec0edc71f53c475296c497
HuacongXie <solarsys@outlook.com>
2025-05-02 01:45:05 -0400

    used response data list to show shop and product list

--- src/containers/Cart/Cart.tsx
+++ src/containers/Cart/Cart.tsx
@@ -50,7 +50,8 @@ function Cart() {
             <div className='title'>
                 Cart
             </div>
-            {/*show every shop*/}
+
+            {/*show every shop iterated by list*/}
             <div className='shop'>
                 <div className='shop-title'>
                     <div className='radio'></div>
@@ -113,69 +114,82 @@ function Cart() {
                     </div>
                 </div>
             </div>
-            {/*show every shop*/}
-            <div className='shop'>
-                <div className='shop-title'>
-                    <div className='radio'></div>
-                    <span className='iconfont'>&#xe7ce;</span>喜梅蔬菜店
-                </div>
-                <div className='shop-products'>
-                    <div className='shop-product'>
-                        <div className='radio'></div>
-                        <img src='http://statics.dell-lee.com/shopping/category-list-1.png' alt='' className='shop-product-img'/>
-                        <div className='shop-product-content'>
-                            <div className='shop-product-title'>
-                                潍坊水果萝卜10斤水果萝卜甜脆水果型潍县青沙窝天津萝卜…
+            {
+                list.map(shop=>{
+                    return (
+
+                        <div className='shop'>
+                            <div className='shop-title'>
+                                <div className='radio'></div>
+                                <span className='iconfont'>&#xe7ce;</span>喜梅蔬菜店
                             </div>
+                            <div className='shop-products'>
+                                <div className='shop-product'>
+                                    <div className='radio'></div>
+                                    <img src='http://statics.dell-lee.com/shopping/category-list-1.png' alt=''
+                                         className='shop-product-img'/>
+                                    <div className='shop-product-content'>
+                                        <div className='shop-product-title'>
+                                            潍坊水果萝卜10斤水果萝卜甜脆水果型潍县青沙窝天津萝卜…
+                                        </div>
 
 
-                            <div className='shop-product-kilo'>
-                                0.43kg
+                                        <div className='shop-product-kilo'>
+                                            0.43kg
+                                        </div>
+                                        <div className='shop-product-price'>
+                                            <span className='shop-product-price-symbol'>&#36; </span>99
+                                        </div>
+                                        <input value='3' className='shop-product-count'/>
+                                    </div>
+                                </div>
+                                <div className='shop-product'>
+                                    <div className='radio'></div>
+                                    <img src='http://statics.dell-lee.com/shopping/category-list-1.png' alt=''
+                                         className='shop-product-img'/>
+                                    <div className='shop-product-content'>
+                                        <div className='shop-product-title'>
+                                            潍坊水果萝卜10斤水果萝卜甜脆水果型潍县青沙窝天津萝卜…
+                                        </div>
+
+
+                                        <div className='shop-product-kilo'>
+                                            0.43kg
+                                        </div>
+                                        <div className='shop-product-price'>
+                                            <span className='shop-product-price-symbol'>&#36; </span>99
+                                        </div>
+                                        <input value='3' className='shop-product-count'/>
+                                    </div>
+                                </div>
+                                <div className='shop-product'>
+                                    <div className='radio'></div>
+                                    <img src='http://statics.dell-lee.com/shopping/category-list-1.png' alt=''
+                                         className='shop-product-img'/>
+                                    <div className='shop-product-content'>
+                                        <div className='shop-product-title'>
+                                            潍坊水果萝卜10斤水果萝卜甜脆水果型潍县青沙窝天津萝卜…
+                                        </div>
+
+
+                                        <div className='shop-product-kilo'>
+                                            0.43kg
+                                        </div>
+                                        <div className='shop-product-price'>
+                                            <span className='shop-product-price-symbol'>&#36; </span>99
+                                        </div>
+                                        <input value='3' className='shop-product-count'/>
+                                    </div>
+                                </div>
                             </div>
-                            <div className='shop-product-price'>
-                                <span className='shop-product-price-symbol'>&#36; </span>99
-                            </div>
-                            <input value='3' className='shop-product-count'/>
                         </div>
-                    </div>
-                    <div className='shop-product'>
-                        <div className='radio'></div>
-                        <img src='http://statics.dell-lee.com/shopping/category-list-1.png' alt='' className='shop-product-img'/>
-                        <div className='shop-product-content'>
-                            <div className='shop-product-title'>
-                                潍坊水果萝卜10斤水果萝卜甜脆水果型潍县青沙窝天津萝卜…
-                            </div>
 
 
-                            <div className='shop-product-kilo'>
-                                0.43kg
-                            </div>
-                            <div className='shop-product-price'>
-                                <span className='shop-product-price-symbol'>&#36; </span>99
-                            </div>
-                            <input value='3' className='shop-product-count'/>
-                        </div>
-                    </div>
-                    <div className='shop-product'>
-                        <div className='radio'></div>
-                        <img src='http://statics.dell-lee.com/shopping/category-list-1.png' alt='' className='shop-product-img'/>
-                        <div className='shop-product-content'>
-                            <div className='shop-product-title'>
-                                潍坊水果萝卜10斤水果萝卜甜脆水果型潍县青沙窝天津萝卜…
-                            </div>
+                    )
+                })
+            }
 
 
-                            <div className='shop-product-kilo'>
-                                0.43kg
-                            </div>
-                            <div className='shop-product-price'>
-                                <span className='shop-product-price-symbol'>&#36; </span>99
-                            </div>
-                            <input value='3' className='shop-product-count'/>
-                        </div>
-                    </div>
-                </div>
-            </div>
             {/* show total price*/}
             <div className='total-price'>
                 <div className='select-all'>
@@ -192,7 +206,7 @@ function Cart() {
                 <div className='check'>结算(2)</div>
 
             </div>
-            <Docker activeName='cart' />
+            <Docker activeName='cart'/>
         </div>
     )
 }
