02aa1adfda0be15f40c1805e13e6284cbaf43b1d
HuacongXie <solarsys@outlook.com>
2025-05-02 01:05:25 -0400

    modified types.ts for Cart.tsx

--- src/containers/Cart/types.ts
+++ src/containers/Cart/types.ts
@@ -1,5 +1,19 @@
 //返回内容类型
 export type ResponseType = {
     success: boolean,
-    data:  Array<{}>,
+    data:  Array<{
+        shopId: string;
+        shopName: string;
+        cartList: Array<{
+            productId: string;
+            imgUrl: string;
+            weight: string;
+            title: string;
+            price: number;
+            count: number;
+        }>;
+
+
+
+    }>,
 }
