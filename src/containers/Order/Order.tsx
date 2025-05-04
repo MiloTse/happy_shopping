3ce5e5bae2a400bff38a7234b693a6f3c08008aa
HuacongXie <solarsys@outlook.com>
2025-05-04 17:04:35 -0400

    added receiver info div on Order.tsx

--- src/containers/Order/Order.tsx
+++ src/containers/Order/Order.tsx
@@ -9,14 +9,16 @@ import './style.scss';
         <div className="page order-page">
             <div className='title'>Confirm Order</div>
             <div className='receiver'>
-
+                <div className='iconfont'> </div>
+                <div className='receiver-content'>
+                    <div className='receiver-name'></div>
+                    <div className='receiver-address'></div>
+                </div>
 
             </div>
-
-         </div>
-    )
-}
-
+        </div>
+ )
+ }
 
 
-export default Order;
+ export default Order;
