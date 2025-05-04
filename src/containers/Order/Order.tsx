56d5db435648546dd72c0740c5cf4dc1c5f528a0
HuacongXie <solarsys@outlook.com>
2025-05-04 18:08:05 -0400

    added style and adjusted

--- src/containers/Order/Order.tsx
+++ src/containers/Order/Order.tsx
@@ -9,11 +9,11 @@ import './style.scss';
         <div className="page order-page">
             <div className='title'>Confirm Order</div>
             <div className='receiver'>
-                <div className='iconfont'> </div>
+                <div className='iconfont'>&#xe68e; </div>
                 <div className='receiver-content'>
                     <div className='receiver-name'>
                          Receiver: Tom
-                        <span className='receiver-phone'>12345678900</span>
+                        <span className='receiver-phone'>1-613-727-4723</span>
                     </div>
                     <div className='receiver-address'>
                         Address: 1385 Woodroffe Avenue, Ottawa, ON, K2G 1V8
