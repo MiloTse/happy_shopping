a05a28a86b4984ec115a041f19ccbf46ed5fd55d
HuacongXie <solarsys@outlook.com>
2025-05-10 00:21:50 -0400

    created timeRange key and value in  orderDetail.json

--- src/containers/Order/Order.tsx
+++ src/containers/Order/Order.tsx
@@ -61,24 +61,7 @@ import { Picker } from 'antd-mobile';
 
      }
 
-       const basicColumns = [
-         [
-             { label: '2025-05-09', value: '2025-05-09' },
-             { label: '2025-05-09', value: '2025-05-09' },
-             { label: '2025-05-09', value: '2025-05-09' },
-         ],
-         [
-             { label: '09', value:'09' },
-             { label: '10', value: '10' },
-             { label: '1', value: '11' },
-         ],
-         [
-             { label: '0', value:'0' },
-             { label: '15', value: '15' },
-             { label: '30', value: '30' },
-             { label: '40', value: '40' },
-         ],
-     ]
+       const basicColumns = [];
 
      return data? (
         <div className="page order-page">
