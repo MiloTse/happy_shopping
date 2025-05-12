054c7368ecaab4f5ddabd70dcd51f7adae677b17
HuacongXie <solarsys@outlook.com>
2025-05-11 22:05:53 -0400

    imported Docker component to Mine.tsx

--- src/containers/Mine/Mine.tsx
+++ src/containers/Mine/Mine.tsx
@@ -1,11 +1,16 @@
 import './style.scss';
+import React from "react";
+import Docker from "../../components/Docker/Docker";
 
 
 
 function Mine() {
     return (
         <div className="mine-page">
-            <h1>Mine</h1>
+            <div className='title'>
+                Mine
+            </div>
+            <Docker activeName='cart'/>
         </div>
     )
 
